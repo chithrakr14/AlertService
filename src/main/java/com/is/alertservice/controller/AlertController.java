00a1a0efe6071353af32a27db30d2b9d78e1c5d3
sajee <sajee@DESKTOP-KSAC2TJ>
2020-05-03 22:23:48 -0400

    changed comment

--- src/main/java/com/is/alertservice/controller/AlertController.java
+++ src/main/java/com/is/alertservice/controller/AlertController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AlertController {
 	
-/*Aler Controller*/	
+/*Aler Controller -- Added by Chithra*/ 	
 	@GetMapping("/alert")
 	public List alert()
 	{
