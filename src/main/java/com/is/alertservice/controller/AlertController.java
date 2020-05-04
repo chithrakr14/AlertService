4bfc03de6f48333887e01983e97f47e9fb810d4f
sajee <sajee@DESKTOP-KSAC2TJ>
2020-05-03 21:54:35 -0400

    Initial commit

---
+++ src/main/java/com/is/alertservice/controller/AlertController.java
@@ -0,0 +1,30 @@
+package com.is.alertservice.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AlertController {
+	
+	
+	@GetMapping("/alert")
+	public List alert()
+	{
+		
+		List<String> list = new ArrayList<String>();
+		list.add("sms");
+		list.add("email");
+		list.add("pushnotification");
+		
+		
+		return list;
+	}
+	
+	
+
+}
